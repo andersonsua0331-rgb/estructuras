ab1bfbc61d3fa06909f434e69ff40bd31d5caaca
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-01-23 02:25:11 +0000

    Actividad 1: Diseño Conceptual (TADs)

---
+++ IPlaylist.java
@@ -0,0 +1,7 @@
+ public interface IPlaylist
+ 
+ void agregarCancion(Cancion c);
+ void eliminarCancion(String titulo);
+ Cancion reproducirSiguiente();
+ void vaciarLista();
+ int obtenerCantidadCanciones();
---
+++ src/Cancion.java
@@ -0,0 +1,13 @@
+public class Cancion{
+
+private int Duracion;
+private String Artista;
+private String Titulo;
+
+public Cancion(int Duracion, String Artista, String Titulo){
+    this.Duracion=Duracion;
+    this.Artista=Artista;
+    this.Titulo=Titulo;
+    }
+    
+
