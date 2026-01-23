f6b3e0777cbc58e4cbd1b839cab1afe8f3655a2e
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-01-23 02:49:21 +0000

    Actividad 1: Diseño Conceptual (TADs)

--- src/Cancion.java
+++ src/Cancion.java
@@ -1,11 +1,10 @@
 public class Cancion{
 
-private int Duracion;
 private String Artista;
 private String Titulo;
 
 public Cancion(int Duracion, String Artista, String Titulo){
-    this.Duracion=Duracion;
+    
     this.Artista=Artista;
     this.Titulo=Titulo;
     }
---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+
+public class Principal{
+    public static void main(String[] args) {
+
+    Cancion Titulo=new  Cancion(3, "jeisson", "el mejor caballo");
+
+    }
+}
